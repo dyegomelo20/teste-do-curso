a5657bab477079e661d840e47a9296e134a84a27
dyegomelo20 <dyegomelo16@gmail.com>
2025-05-16 20:14:01 -0300

    Aprendedo algums coisas no css

---
+++ altenalogineregistro/javascrip.js
@@ -0,0 +1,11 @@
+
+  function mostrarRegistro() {
+    document.getElementById("login-form").classList.remove("active");
+    document.getElementById("registro-form").classList.add("active");
+  }
+
+  function mostrarLogin() {
+    document.getElementById("registro-form").classList.remove("active");
+    document.getElementById("login-form").classList.add("active");
+  }
+
